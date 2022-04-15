2ec9fd39f89cc6c4aa36c9979ae84cbbf3e819b4
Kaushal Vaja <vajakaushal@gmail.com>
2022-04-15 16:05:18 +0530

    create db

--- scripts/create_test_db.sh
+++ scripts/create_test_db.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-mysql -uroot  << create database bagisto;
-CREATE USER 'bagistouser'@'localhost' IDENTIFIED BY 'bagisto';
-GRANT ALL ON bagisto.* TO 'bagistouser'@'localhost' WITH GRANT OPTION;
-flush privileges;
-exit;
+#mysql -uroot  << create database bagisto;
+#CREATE USER 'bagistouser'@'localhost' IDENTIFIED BY 'bagisto';
+#GRANT ALL ON bagisto.* TO 'bagistouser'@'localhost' WITH GRANT OPTION;
+#flush privileges;
+#exit;
 
+
+mysql -uroot  -e "create database bagisto;"
+mysql -uroot -e "CREATE USER 'bagistouser'@'localhost' IDENTIFIED BY 'bagisto';"
+mysql -uroot -e "GRANT ALL ON bagisto.* TO 'bagistouser'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -e "flush privileges;"
+#exit;
