969c2bb1295e54714373671eb5ad6452d3e04365
Kaushal Vaja <vajakaushal@gmail.com>
2022-04-15 16:50:45 +0530

    composer file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-apt update 
-apt install -y apache2 
+apt update
+apt install -y apache2
 systemctl start apache2.service
 systemctl enable apache2.service
 
-apt install -y software-properties-common 
-echo -ne '\n' | add-apt-repository ppa:ondrej/php 
+apt install -y software-properties-common
+echo -ne '\n' | add-apt-repository ppa:ondrej/php
 
-apt update 
+apt update
 apt install -y php7.3 php7.3-{common,gmp,curl,soap,bcmath,intl,mbstring,xmlrpc,mysql,gd,xml,cli,zip} libapache2-mod-php7.3
 
 php -v
