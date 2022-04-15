67fa5052c46dbeab6a083d742514b3e0b53d5e8f
Kaushal Vaja <vajakaushal@gmail.com>
2022-04-15 15:49:37 +0530

    install dependancies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,11 +5,11 @@ apt install -y apache2
 systemctl start apache2.service
 systemctl enable apache2.service
 
-apt-get install -y software-properties-common 
+apt install -y software-properties-common 
 add-apt-repository ppa:ondrej/php 
 
-apt-get update 
-apt-get install -y php7.3 php7.3-{common,gmp,curl,soap,bcmath,intl,mbstring,xmlrpc,mysql,gd,xml,cli,zip} libapache2-mod-php7.3
+apt update 
+apt install -y php7.3 php7.3-{common,gmp,curl,soap,bcmath,intl,mbstring,xmlrpc,mysql,gd,xml,cli,zip} libapache2-mod-php7.3
 
 php -v
 a2enmod php7.3
@@ -17,8 +17,8 @@ a2enmod rewrite
 service apache2 restart
 
 
-apt-get install -y curl
+apt install -y curl
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-apt-get install -y nodejs
+apt install -y nodejs
 
-apt-get install mysql-server -y
+apt install -y mysql-server
