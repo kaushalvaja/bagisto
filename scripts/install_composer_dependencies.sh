ee2d380b781e4e3c81ab330afc3347223bba431e
ritabhatti <ritabhatti123@gmail.com>
2022-04-18 11:35:14 +0530

    install dependency sh

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,3 +1,9 @@
 apt install git
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 composer -V
+
+chown -R bagisto:bagisto /home/bagisto
+
+cd /home/bagisto/bagisto/public
+
+composer install
