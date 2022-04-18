0392e75160720ca012e820aeaadb659b5131386d
ritabhatti <ritabhatti123@gmail.com>
2022-04-18 11:47:06 +0530

    php ini

--- scripts/php_ini.sh
+++ scripts/php_ini.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp /etc/php/7.3/apache2/php.ini /etc/php/7.3/apache2/php.ini_orig
+cp /etc/php/7.4/apache2/php.ini /etc/php/7.4/apache2/php.ini_orig
 
 echo "file_uploads = On
 allow_url_fopen = On
@@ -8,4 +8,4 @@ short_open_tag = On
 memory_limit = 256M
 upload_max_filesize = 100M
 max_execution_time = 360
-date.timezone = America/Chicago " >  /etc/php/7.3/apache2/php.ini
+date.timezone = America/Chicago " >  /etc/php/7.4/apache2/php.ini
